6866d820f7d940ba7b5e7b2db7f4cac2fd8f73cc
leigt <leigt@leigtdeMacBook-Pro.local>
2018-09-13 18:13:11 +0800

    Initialized

---
+++ data-structure-algorithm/src/main/java/basics/dsa/Main.java
@@ -0,0 +1,11 @@
+package basics.dsa;
+
+/**
+ * Created by leigt on 2018/9/13.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hi, algorithm");
+    }
+}
