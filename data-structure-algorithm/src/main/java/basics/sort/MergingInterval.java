9b682992bb6fc462c0ff3dbccd6a76adbdccc77e
Lei Guoting <tonyley3@gmail.com>
2019-04-30 13:42:12 +0800

    Fix intervals merge

--- data-structure-algorithm/src/main/java/basics/sort/MergingInterval.java
+++ data-structure-algorithm/src/main/java/basics/sort/MergingInterval.java
@@ -12,6 +12,10 @@ package basics.sort;
  * 输入: [[1,4],[4,5]]
  * 输出: [[1,5]]
  * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * <p>
+ * 例3:
+ * 输入:[[2,3],[4,5],[6,7],[8,9],[1,10]]
+ * 输出:[[1,10]]
  * Created by Lei Guoting on 2019/4/25.
  */
 public class MergingInterval {
@@ -20,38 +24,10 @@ public class MergingInterval {
         int length = intervals.length;
         int[][] merged = new int[length][];
         int mergedIndex = 0;
-        for (int i = 0; i < length; ) {
-            int[] first = intervals[i];
-            for (int j = i + 1; j < length; ) {
-                int[] compared = intervals[j];
-                if (first[1] > compared[0]) {
-                    if (first[1] > compared[1]) {
-                        j++;
-                    } else {
-                        merged[mergedIndex++] = new int[]{
-                                first[0],
-                                compared[1]
-                        };
-                        i = j + 1;
-                        break;
-                    }
-                } else if (first[1] == compared[0]) {
-                    merged[mergedIndex++] = new int[]{
-                            first[0],
-                            compared[1]
-                    };
-                    i = j + 1;
-                    break;
-                } else {
-                    merged[mergedIndex++] = first;
-                    i = j;
-                    break;
-                }
-            }
-
-            if (i == length - 1) {
-                merged[mergedIndex++] = intervals[i];
-                i++;
+        for (int i = 0; i < length; i++) {
+            int[] merge = mergeInner(intervals, i);
+            if (merge.length != 0) {
+                merged[mergedIndex++] = merge;
             }
         }
 
@@ -61,31 +37,86 @@ public class MergingInterval {
             System.arraycopy(merged, 0, newArray, 0, realLength);
             merged = newArray;
         }
-
+        sort(merged);
         return merged;
     }
 
-    public static int [][] merge2(int[][] intervals){
+    protected static int[] mergeInner(int[][] intervals, int startIndex) {
         int length = intervals.length;
-        int[][] merged = new int[length][];
-        int mergedIndex = 0;
-        for (int i = 0; i < length; ) {
-            int[] start = intervals[i];
-            for (int j = i + 1; j < length; ) {
-                int[] next = intervals[j];
+        int[] start = intervals[startIndex];
+        if (startIndex == length - 1) {
+            return start;
+        }
+        if (start.length == 0) {
+            return start;
+        }
+
+        for (int i = startIndex + 1; i < length; i++) {
+            int[] next = intervals[i];
+            int nextLength = next.length;
+            if (nextLength == 0) {
+                continue;
+            }
+
+            if (compare(start, next) == 0) {
+                start[0] = Math.min(start[0], next[0]);
+                start[1] = Math.max(start[1], next[1]);
+                intervals[i] = new int[0];
             }
         }
-        return null;
+        return start;
+    }
+
+    private static int compare(int[] left, int[] right) {
+        if (left[1] < right[0]) {
+            return -1;
+        } else if (left[0] > right[1]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static void sort(int[][] intervals) {
+        int length = intervals.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                int[] first = intervals[j];
+                int[] second = intervals[j + 1];
+                if (compare(first, second) > 0) {
+                    intervals[j] = second;
+                    intervals[j + 1] = first;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
+        /**
+         *
+         * [[1,4],[0,4]]
+         * [[1,4],[0,0]]
+         * [[4,5],[1,4],[0,1]]
+         */
+
         int source[][] = new int[][]{
+                new int[]{8, 10},
                 new int[]{1, 3},
                 new int[]{2, 6},
-                new int[]{8, 10},
                 new int[]{15, 18}
         };
-        int[][] result = merge(source);
+
+        int source2[][] = new int[][]{
+                new int[]{1, 4},
+                new int[]{0, 0}
+        };
+
+        int source3[][] = new int[][]{
+                new int[]{4, 5},
+                new int[]{1, 4},
+                new int[]{0, 1},
+        };
+        int[][] result = merge(source3);
         int length = result.length;
         for (int i = 0; i < length; i++) {
             int[] second = result[i];
@@ -98,5 +129,8 @@ public class MergingInterval {
                 System.out.println(builder.toString());
             }
         }
+
+        int compared = compare(new int[]{1, 5}, new int[]{0, 1});
+        System.out.println(String.format("compared: %s", Integer.toString(compared)));
     }
 }
